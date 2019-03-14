32b0cd58e170288436a90f0ae34ea47199a7b12e
Suleiman <suleiman.azhari@gmail.com>
2019-03-14 09:48:21 +0800

    Bind Data to UI
    Signed-off-by: Suleiman <suleiman.azhari@gmail.com>

--- app/src/main/java/com/suleimanazhari/openweather/ui/WeatherActivity.kt
+++ app/src/main/java/com/suleimanazhari/openweather/ui/WeatherActivity.kt
@@ -1,8 +1,10 @@
 package com.suleimanazhari.openweather.ui
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.suleimanazhari.openweather.R
@@ -14,7 +16,7 @@ import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
 class WeatherActivity : AppCompatActivity(), CoroutineScope {
-    
+
     private lateinit var job: Job
     private lateinit var viewModel: WeatherViewModel
 
@@ -40,8 +42,36 @@ class WeatherActivity : AppCompatActivity(), CoroutineScope {
         temp.observe(this@WeatherActivity, Observer {
             if (it == null) return@Observer
 
-            textView_temperature.text = it.main.temp.toString()
+            group_loading.visibility = View.GONE
+            showCity(it.name)
+            showTemp(it.main.temp)
+            showDesc(it.weather[0].description)
+            showMinTemp(it.main.tempMin)
+            showMaxTemp(it.main.tempMax)
         })
+    }
+
+    private fun showCity(city: String) {
+        textView_city.text = city
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun showTemp(temp: Double) {
+        textView_temperature.text = "$temp°C"
+    }
+
+    private fun showDesc(desc: String) {
+        textView_description.text = desc
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun showMinTemp(temp: Double) {
+        textView_temp_min.text = "Minimum Temperature: $temp°C"
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun showMaxTemp(temp: Double) {
+        textView_temp_max.text = "Minimum Temperature: $temp°C"
     }
 
     override val coroutineContext: CoroutineContext
